daf859ae9b3e7ad6874c72e68e61a6bc24861ce4
selftech <selftechacademy@gmail.com>
2023-07-03 22:49:10 -0500

    maxChar and polyndrome test added

---
+++ exercises/maxchar/index.js
@@ -0,0 +1,17 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+//NOTE:This solution will be also good for questions like?
+/*
+1. What is the most common character in the string
+2. Does String A has same characters as B?
+3.Does that character have any repeated characters in it?
+
+*/
+function maxChar(str) {}
+
+module.exports = maxChar;
